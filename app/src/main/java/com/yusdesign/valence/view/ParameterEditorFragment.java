61e084e2d6bcab1197e8c6477d3042bdd3b6eb5c
yus <swiftcase@gmail.com>
2026-08-11 23:17:21 +0800

    Update ParameterEditorFragment.java

--- app/src/main/java/com/yusdesign/valence/view/ParameterEditorFragment.java
+++ app/src/main/java/com/yusdesign/valence/view/ParameterEditorFragment.java
@@ -1,4 +1,4 @@
-package com.nathaniel.motus.umlclasseditor.view;
+package com.yusdesign.valence.view;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -18,13 +18,13 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.nathaniel.motus.umlclasseditor.R;
-import com.nathaniel.motus.umlclasseditor.model.MethodParameter;
-import com.nathaniel.motus.umlclasseditor.model.TypeMultiplicity;
-import com.nathaniel.motus.umlclasseditor.model.TypeNameComparator;
-import com.nathaniel.motus.umlclasseditor.model.UmlClass;
-import com.nathaniel.motus.umlclasseditor.model.UmlClassMethod;
-import com.nathaniel.motus.umlclasseditor.model.UmlType;
+import com.yusdesign.valence.R;
+import com.yusdesign.valence.model.MethodParameter;
+import com.yusdesign.valence.model.TypeMultiplicity;
+import com.yusdesign.valence.model.TypeNameComparator;
+import com.yusdesign.valence.model.UmlClass;
+import com.yusdesign.valence.model.UmlClassMethod;
+import com.yusdesign.valence.model.UmlType;
 
 import java.util.ArrayList;
 import java.util.Collections;
